4a093855c3f499ccb13238330b5a4e61c86e7f1c
Dhea Angelina <dhea.angelina@ti.ukdw.ac.id>
2021-11-11 15:21:04 +0700

    3_A_71210693

---
+++ 3_A_71210693.py
@@ -0,0 +1,7 @@
+db1 = input("Masukkan daftar belanja 1: ")
+db2 = input("Masukkan daftar belanja 2: ")
+ddb1 = input("Tambahkan data ke daftar belanja 1: ")
+ddb2 = input("Tambahkan data ke daftar belanja 2: ")
+
+print("Daftar belanja 1 adalah " + db1 + "," + ddb1)
+print("Daftar belanja 2 adalah " + db2 + "," + ddb2)
